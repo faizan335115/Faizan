33d73f8984d33bacd10cfd2509ff478d0cbf9ead
Tabish <Tabish@RAJPOOT>
2022-06-29 19:15:30 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Faizan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
